6c901f1e3de5e61d9fd0a1fc7d76c3924a0ff5db
gsmoy <gsmoy@DESKTOP-N633PBP>
2024-03-02 00:25:54 -0600

    He agredado la variable nombre

--- src/Main.java
+++ src/Main.java
@@ -8,40 +8,45 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+
+
+
 		/*
 		 * creo un try para manejar excepciones
 		 */
-		try {
+		try {		
 			// creo un scanner para guardar los datos del usuario
 			Scanner entrada = new Scanner(System.in);
+			System.out.println("Dime tu nombre");
+			String nombre = entrada.nextLine();
 			System.out.println("Dime tu edad");
 			int edadAct = entrada.nextInt();
-			
 			/*
 			 * accede al metodo de la clase persona y se devuelve el resultado en la variable edadJub
 			 */
-			Persona persona = new Persona(edadAct);
+			Persona persona = new Persona(edadAct, nombre);
 			int edadJub =persona.calcularTiempoHastaJubilacion();
 			
 			/*
 			 *  ejecuta el if correspondiente dependiendo de la variable edadJub
 			 */
 			if (edadJub <= 0) { //si menor o igual a cero
-				System.out.println("¡Jubilate!  Te has pasado " + Math.abs(edadJub) + " año/s" );
+				System.out.println( nombre + "¡Jubilate!  Te has pasado " + Math.abs(edadJub) + " año/s" );
 			}
 			else if (edadJub>=10) { //si mayor o igual a 10
-				System.out.println("Te quedan " + edadJub + " años. ¡A seguir trabajando gandúl!");
+				System.out.println(nombre +"Te quedan " + edadJub + " años. ¡A seguir trabajando gandúl!");
 			}
 			else if (edadJub <10){ // si menor a 10
-				System.out.println ("Sólo te quedan " + edadJub + " años. ¡Vamos que ya queda poco!");
-			}
-		}
+				System.out.println (nombre +"Sólo te quedan " + edadJub + " años. ¡Vamos que ya queda poco!");
+			} 
+		} 
 		/**
 		 * si ocurre un fallo se proporciona un mensaje de error
 		 * @exception si ocurre algun fallo lanza el mensaje
 		 */
 		catch (Exception ex) {
+			
 			System.out.println("Error. Algo ha pasado. ¿Has escrito un numero en tu edad?");
-		}
+		}  		  
 	} 
-}
+}	
--- src/Persona.java
+++ src/Persona.java
@@ -9,15 +9,17 @@ public class Persona {
      * @param edadAct La edad actual de la persona.
      */
 	int edadAct;
+	String nombre;
 	// metodo construcctor
-	public Persona(int edadAct)  {
+	public Persona(int edadAct, String nombre)  {
 		this.edadAct=edadAct;
-	}
+		this.nombre=nombre;	}
 	/**
 	 declaracion del metodo para calcular la diferencia de años con la jubilacion
 	 * @return
 	 */
     public  int calcularTiempoHastaJubilacion() {
         return 67 - edadAct;
-    }		
+    }
+    
 }
